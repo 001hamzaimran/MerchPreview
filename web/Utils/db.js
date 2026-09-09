ef644e0d17c0f24d54d6bfb2cc0a0e5e2287e9f9
001hamzaimran <syed.hamza2004@gmail.com>
2026-09-09 23:09:25 +0500

    set DB

--- web/Utils/db.js
+++ web/Utils/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/merch-preview";
+  const uri = process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.scibxar.mongodb.net/merch-preview";
   try {
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
