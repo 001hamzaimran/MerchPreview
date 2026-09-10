435f31bf5da4f4a68b122639511218249c8acc87
001hamzaimran <syed.hamza2004@gmail.com>
2026-09-11 00:37:19 +0500

    push heroku:

--- web/Utils/db.js
+++ web/Utils/db.js
@@ -2,8 +2,8 @@ import "./polyfill.js";
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  // mongodb+srv://admin:admin@cluster0.scibxar.mongodb.net/merch-preview
-  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/merch-preview";
+  // mongodb://localhost:27017/merch-preview
+  const uri = process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.scibxar.mongodb.net/merch-preview";
   try {
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
